bc1019009e076ddbbd78e037fb506d3ce317de0c
aryan <aryanprofessional498@gmail.com>
2025-02-13 12:02:44 +0530

    js code as finally showing output on the ui

--- final/node.js
+++ final/node.js
@@ -1,56 +1,34 @@
-const apiKey = "cad7559e22b909c6eaa6574aaed98657"
-
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid="
-
-let loc = document.querySelector(".loc")
-let temp = document.querySelector(".tempC")
-let humid = document.querySelector(".humid")
-let wind = document.querySelector(".wind")
-let weatherC = document.querySelector(".weatherC")
-let datee = document.querySelector(".date")
-let place = "jaipur"
-
-let abc = function storeValue() {
-   // Get the input element
-   let inputElement = document.querySelector('.myInput');
-      
-   // Store the value in a variable
-   let storedValue = inputElement.value;
+function storeInput() {
+   const apiKey = "cad7559e22b909c6eaa6574aaed98657";
+   const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=";
    
-   return storedValue
-}
-
-console.log(abc)
-
-//problem: able to wirte place in input field and get the data on card
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function checkWeather() {
-   const response = await fetch(apiUrl + apiKey + "&q=" + place)
-   var data = await response.json();
-   console.log(data);
-
-   loc.innerHTML = data.name
-   temp.innerHTML = Math.floor(data.main.temp) + "°C"
-   humid.innerHTML = data.main.
-   humidity + "%"
-
-   wind.innerHTML = Math.floor(data.wind.speed) + "m/s"
+   let loc = document.querySelector(".loc");
+   let temp = document.querySelector(".tempC");
+   let humid = document.querySelector(".humid");
+   let wind = document.querySelector(".wind");
+   let weatherC = document.querySelector(".weatherC");
+   let datee = document.querySelector(".date"); // Ensure you have an element with this class
    
-   weatherC.innerHTML = data.weather[0].main
-   datee.innerHTML = (new Date(data.dt * 1000).toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' })); // Output: "Sun, Feb 09"
-
+   var inputValue = document.getElementById("myInput").value;
+   // console.log(inputValue);
+   let place = inputValue;
+   
+   async function checkWeather() {
+       const response = await fetch(apiUrl + apiKey + "&q=" + place);
+       var data = await response.json();
+      //  console.log(data);
+       
+       if (data.cod === "404") {
+           alert("City not found! Please try again.");
+           return;
+       }
+       
+       loc.innerHTML = data.name + ", ";
+       temp.innerHTML = Math.floor(data.main.temp) + "°C";
+       humid.innerHTML = data.main.humidity + "%";
+       wind.innerHTML = Math.floor(data.wind.speed) + "m/s";
+       weatherC.innerHTML = data.weather[0].main;
+       datee.innerHTML = (new Date(data.dt * 1000).toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' }));
+   }
+   checkWeather();
 }
-
-checkWeather();
